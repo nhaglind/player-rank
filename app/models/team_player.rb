bdef613d2ac75089461e0c29b3329bf2a48fd29f
Nick Haglind <nhaglind@gmail.com>
2018-04-24 14:23:54 -0500

    add validation for edit and upgrade to hash syntax

--- app/models/team_player.rb
+++ app/models/team_player.rb
@@ -1,4 +1,6 @@
 class TeamPlayer < ApplicationRecord
   belongs_to :team
   belongs_to :player
+  default_scope { order(position: :asc) }
+  validates :player_id, uniqueness: { scope: :team_id }
 end
