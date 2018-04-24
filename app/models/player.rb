c836a11f01286ca5a712a92ff0547b1ddd9505da
Nick Haglind <nhaglind@gmail.com>
2018-04-24 15:20:45 -0500

    dumb migration issue remember to create the tables

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
 class Player < ApplicationRecord
   has_many :team_players
+  has_many :tournaments, through: :player_tournaments
   has_many :teams, through: :team_players
 end
--- db/migrate/20180424200420_create_tournament_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateTournamentTable < ActiveRecord::Migration[5.1]
-  def change
-    create_table :tournaments do |t|
-      t.string  "tournament_name"
-      t.integer "tournament_year"
-      t.timestamps
-    end
-  end
-end
