d2ce8982abe2718eb3c654bc67da091fbce890ad
Nick Haglind <nhaglind@gmail.com>
2018-04-24 15:27:30 -0500

    remove unused migrations just run db reset and seed that stuff

--- db/migrate/20180418171358_add_full_name_to_players.rb
+++
@@ -1,7 +0,0 @@
-class AddFullNameToPlayers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :players, :full_name, :string
-    remove_column :players, :first_name
-    remove_column :players, :last_name
-  end
-end
--- db/migrate/20180424160500_remove_index_on_team_players.rb
+++
@@ -1,7 +0,0 @@
-class RemoveIndexOnTeamPlayers < ActiveRecord::Migration[5.1]
-  def change
-    remove_index "team_players", ["player_id"]
-    remove_index "team_players", ["team_id", "position"]
-    remove_index "team_players", ["team_id", "player_id"]
-  end
-end
